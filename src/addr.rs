867ca27a9a7ea3f6bac60ed91da7dd3c531253fb
ZK Clay <hi@zkclay.com>
2024-04-03 21:01:52 -0400

    QoL changes.

--- src/addr.rs
+++ src/addr.rs
@@ -6,7 +6,50 @@ use rand_chacha::rand_core::SeedableRng;
 use dusk_pki::SecretSpendKey;
 use dusk_bytes::Serializable;
 
-pub fn generate_addr() -> (String, String, String, String) {
+use std::fs::OpenOptions;
+use std::io::Write;
+use std::io::Result;
+
+pub fn find_vanity_address(prefix: String, case_sensitive: bool, file_path: &str) {
+    let mut counter = 0;
+    let interval = 100;
+
+    loop {
+        let result = generate_addr();
+        let addr_string = if case_sensitive {
+            result.1.clone().to_lowercase()
+        } else {
+            result.1.clone()
+        };
+    
+        if addr_string.to_lowercase().starts_with(&prefix) {
+            println!("Address found.");
+            append_tuple_to_file(result, file_path).expect("error writing to file");
+        }
+        
+        if counter % interval == 0 {
+            println!("{} addresses searched.", counter);
+        }
+
+        counter += 1;
+    }
+}
+
+fn append_tuple_to_file(tuple: (String, String, String, String), file_path: &str) -> Result<()> {
+    let combined_string = format!("{},{},{},{}\n", tuple.0, tuple.1, tuple.2, tuple.3);
+
+    // Open the file in append mode, creating it if it does not exist
+    let mut file = OpenOptions::new()
+        .append(true)
+        .create(true)
+        .open(file_path)?;
+
+    file.write_all(combined_string.as_bytes())?;
+
+    Ok(())
+}
+
+fn generate_addr() -> (String, String, String, String) {
     let seed_info = create_seed_from_memonmic();
     let keys = create_spend_keys_from_seed(&seed_info.0, 0);
     (seed_info.1, keys.0, keys.1, keys.2)
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,9 @@
+
 mod addr;
 
 fn main() {
-    let prefix = String::from("zk");
-    let case_sensitive = false; 
-
-    loop {
-        let result = addr::generate_addr();
-        let addr_string = if case_sensitive {
-            result.1.clone().to_lowercase()
-        } else {
-            result.1.clone()
-        };
-    
-        if addr_string.to_lowercase().starts_with(&prefix) {
-            println!("{:?}", &result);
-        }
-    }
+    let prefix = String::from("q");
+    let case_sensitive = false;
+    let file_path = "results.txt";
+    addr::find_vanity_address(prefix, case_sensitive, file_path);
 }
