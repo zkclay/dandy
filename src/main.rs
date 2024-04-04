54ee77573a5a5253b610e80255f4600ff761766a
ZK Clay <hi@zkclay.com>
2024-04-03 21:11:12 -0400

    add command line arguments.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,24 @@
+use std::env;
 
 mod addr;
 
+fn parse_case_sensitive(arg: &str) -> bool {
+    matches!(arg.to_lowercase().as_str(), "true" | "t" | "yes" | "y" | "1")
+}
+
 fn main() {
-    let prefix = String::from("q");
-    let case_sensitive = false;
-    let file_path = "results.txt";
-    addr::find_vanity_address(prefix, case_sensitive, file_path);
+
+    let args: Vec<String> = env::args().collect();
+
+    // Check if we got the correct number of arguments (4 in total, including the program name)
+    if args.len() < 4 {
+        eprintln!("Usage: {} <prefix> <caseSensitive?> <resultFilePath>", args[0]);
+        std::process::exit(1);
+    }
+
+    let prefix = &args[1];
+    let case_sensitive = parse_case_sensitive(&args[2]);
+    let file_path = &args[3];
+
+    addr::find_vanity_address(prefix.to_string(), case_sensitive, file_path);
 }
